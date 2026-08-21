5f091ca7b52d1cc8dc23e4dc4b5946f5ecaee67e
DarshSrivastava04 <darsh.srivastava04@gmail.com>
2026-08-21 12:05:33 +0530

    Update 3_payment_processing_system.py
    Added a user interface for selecting payment methods and processing payments.

--- 3_payment_processing_system.py
+++ 3_payment_processing_system.py
@@ -2,28 +2,35 @@ from abc import ABC, abstractmethod
 
 class PaymentStrategy(ABC):
     @abstractmethod
-    def pay(self,amount) -> str:
+    def pay(self, amount) -> str:
         pass
 
+
 class CreditCardPayment(PaymentStrategy):
-    def pay(self,amount) -> str:
+    def pay(self, amount) -> str:
         return f"Processing Credit Card Payment of {amount}"
 
+
 class BitcoinPayment(PaymentStrategy):
-    def pay(self,amount) -> str:
+    def pay(self, amount) -> str:
         return f"Proceessing BitCoin Payment of {amount}"
 
+
 class PayPalPayment(PaymentStrategy):
-    def pay(self, amount)-> str:
+    def pay(self, amount) -> str:
         return f"Processing PayPal Payment of {amount}"
 
+
 class PaymentProcessor:
-    def __init__(self,strategy):
+    def __init__(self, strategy):
         self.strategy = strategy
-    def set_strategy(self,strategy):
+
+    def set_strategy(self, strategy):
         self.strategy = strategy
+
     def process_payment(self, amount):
         return self.strategy.pay(amount)
+
 
 if __name__ == "__main__":
     credit_card = CreditCardPayment()
@@ -31,11 +38,42 @@ if __name__ == "__main__":
     paypal = PayPalPayment()
 
     processor = PaymentProcessor(credit_card)
-    print("\n--- Testing Payment System ---")
-    print(processor.process_payment(100))
 
-    processor.set_strategy(paypal)
-    print(processor.process_payment(250))
+    while True:
+        print("\n---- Payment Platform ----")
+        print("Choose the Payment Method convenient to you from below.")
+        print("1.Credit Card")
+        print("2.BitCoin")
+        print("3.PayPal")
+        print("4.exit")
 
-    processor.set_strategy(bitcoin)
-    print(processor.process_payment(500))
+        try:
+            choice = int(input("Enter serial No. of your choice :"))
+
+            if choice == 4:
+                print("Exiting the program Gracefully.")
+                break
+
+            if choice not in [1, 2, 3]:
+                continue
+
+            amount = int(input("Enter the amount you want to Process :"))
+
+            if choice == 1:
+                processor.set_strategy(credit_card)
+                print(processor.process_payment(amount))
+
+            elif choice == 2:
+                processor.set_strategy(bitcoin)
+                print(processor.process_payment(amount))
+
+            elif choice == 3:
+                processor.set_strategy(paypal)
+                print(processor.process_payment(amount))
+
+            else:
+                print("Please enter among choices provided.")
+
+        except ValueError:
+            print("Please Enter valid values")
+            continue
